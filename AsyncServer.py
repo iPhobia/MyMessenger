1b2ec21ca8555499e3486f200f74a40dc94275d7
Phobia <pashaphb@gmail.com>
2017-11-27 01:08:01 +0200

    added simple GUI

--- AsyncServer.py
+++ AsyncServer.py
@@ -1,6 +1,6 @@
-import socket
 import threading
 import socketserver
+
 
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
 
@@ -9,34 +9,27 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
         cur_thread = threading.current_thread()
         response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
         self.request.sendall(response)
+        print(data)
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
 
-def client(ip, port, message):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((ip, port))
-        sock.sendall(bytes(message, 'ascii'))
-        response = str(sock.recv(1024), 'ascii')
-        print("Received: {}".format(response))
+
 
 if __name__ == "__main__":
-    # Port 0 means to select an arbitrary unused port
+
     HOST, PORT = "localhost", 14900
-
     server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
-    with server:
-        ip, port = server.server_address
-
+    ip, port = server.server_address
         # Start a thread with the server -- that thread will then start one
         # more thread for each request
-        server_thread = threading.Thread(target=server.serve_forever)
+    server_thread = threading.Thread(target=server.serve_forever)
         # Exit the server thread when the main thread terminates
-        server_thread.daemon = True
-        server_thread.start()
-        print("Server loop running in thread:", server_thread.name)
-
-        server.serve_forever()
+    server_thread.daemon = True
+    server_thread.start()
+    print("Server loop running in thread:", server_thread.name)
+    print("Server is running on address:", server.server_address)
+    server.serve_forever()
 
         #https://docs.python.org/3/library/socketserver.html#module-socketserver
         #https://docs.python.org/3.6/library/threading.html#module-threading
--- client2.py
+++ client2.py
@@ -1,7 +1,35 @@
 import socket
+import tkinter as tk
+from tkinter import *
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect(('localhost', 14900))
-    sock.sendall(bytes('Hello AsyncServer', 'ascii'))
-    response = str(sock.recv(1024), 'ascii')
-    print("Received: {}".format(response))
+
+def connect(event):
+    with socket.socket() as conn:
+        conn.connect(("localhost", 14900))
+        conn.send(b"client2")
+        rawdata = conn.recv(1024)
+        data = rawdata.decode("utf-8")
+        textbox.insert(1.0, data + '\n')
+
+root = tk.Tk()
+root.geometry('400x400')
+root.title("Messenger")
+panelFrame = Frame(root, height = 60, bg = 'gray')
+textFrame = Frame(root, height = 50, width = 50)
+
+panelFrame.pack(side = 'top', fill = 'x')
+textFrame.pack(side = 'bottom', fill = 'both', expand = 1)
+
+textbox = Text(textFrame, font='Arial 14', wrap='word')
+scrollbar = Scrollbar(textFrame)
+
+scrollbar['command'] = textbox.yview
+textbox['yscrollcommand'] = scrollbar.set
+
+textbox.pack(side = 'left', fill = 'both', expand = 1)
+scrollbar.pack(side = 'right', fill = 'y')
+
+sendBtn = Button(panelFrame, text = 'Send')
+sendBtn.bind('<Button-1>', connect)
+sendBtn.place(x = 10, y = 10, width = 40, height = 40)
+root.mainloop()
